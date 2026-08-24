91e9e9ad7cb2c3ea6f1bcb25c0e525eaecf27fa7
rakmahefa <rakmahefa@gmail.com>
2026-08-24 18:39:53 +0300

    feat(ui): expose History page

--- src/ui/pages/mod.rs
+++ src/ui/pages/mod.rs
@@ -1,10 +1,12 @@
 mod browse;
+mod history;
 mod home;
 mod library;
 mod manga_detail;
 mod updates;
 
 pub use browse::{BrowsePage, BrowseState};
+pub use history::{HistoryPage, HistoryFilter, HistoryState};
 pub use home::HomePage;
 pub use library::LibraryPage;
 pub use manga_detail::{MangaDetailPage, MangaDetailState};
