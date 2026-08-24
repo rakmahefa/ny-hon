256d75fb61fca7126f2a0e6819d63cf9ab002134
rakmahefa <rakmahefa@gmail.com>
2026-08-24 18:13:19 +0300

    feat(ui): expose Manga Detail page

--- src/ui/pages/mod.rs
+++ src/ui/pages/mod.rs
@@ -1,7 +1,9 @@
 mod home;
 mod library;
+mod manga_detail;
 mod updates;
 
 pub use home::HomePage;
 pub use library::LibraryPage;
+pub use manga_detail::{MangaDetailPage, MangaDetailState};
 pub use updates::UpdatesPage;
